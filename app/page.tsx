7622fe1478e93810802b1901c0d07fc44c2ceced
tty007 <c7dc007@gmail.com>
2023-04-22 21:23:29 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Next.js'
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
