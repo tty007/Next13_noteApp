146b3b8b55ea805238aec2cd33471d196e87acf7
tty007 <c7dc007@gmail.com>
2023-04-25 20:59:15 +0900

    アプリ作成の下準備完了

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    // 指定したファイルのみにtailwindcssが適用されるようにする
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
